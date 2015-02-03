fbc4acd1177b51b291ad2b8764f0156b502f9d91
Joe Phinazee <jpaito08@gmail.com>
2015-02-03 10:45:18 -0800

    Almost final version!!! Just about there!

--- RollTheDiceBeta.java
+++ RollTheDiceBeta.java
@@ -21,10 +21,10 @@ public class RollTheDiceBeta {
 		int five = 0;
 		int six = 0;
 		int streakStart = 0;	// Records the start of the run
-		int currentStreak = 0;	// Records the current run
+		int currentStreak = 1;	// Records the current run
 		int maxStreak = 0;		// The holds the largest streak as the array iterates
 
-		// Allows user to allocate the size of the array
+		// Allows user to choose the size of the array
 		System.out.print("\nPlease enter the number of times you would like to roll the dice: ");
 		numberRolls = TextIO.getInt();
 		// numberRolls = Math.abs(numberRolls); This is one way to do it
@@ -38,18 +38,18 @@ public class RollTheDiceBeta {
 		// This is an assignment statement that sets the size of the array
 		// to the size of rolls the user inputs
 		diceArray = new int[numberRolls];
-		
+
 		// This loops through each index of the array and generates a pseudo random number
 		// to simulate a dice roll
 		for (i = 0; (i < diceArray.length); i++ ){
 			// calculate the value for each die
 			diceArray[i]=(int)((Math.random()*6)+1);
-			
+
 			// If you want to mute the output of each roll just add a comment in
 			// front of the next two lines
 			System.out.printf("Roll #" + i + "  face of the die is " + diceArray[i]);
 			System.out.println();
-			
+
 			// These if statements count the number of times each number appears on the face of the die
 			if (diceArray[i] == 1){
 				one = one + 1;
@@ -69,23 +69,23 @@ public class RollTheDiceBeta {
 			if (diceArray[i] == 6){
 				six = six + 1;
 			}
-			
-		    // This is compares the current record to the previous record 
+
+			// This is compares the current record to the previous record 
 			// in the array in order to count the run and longest streak
-			
+
 			if (i != 0){
-			if (diceArray[i] == diceArray[i-1]) { // never compare a boolean with a boolean constant, just use it
-		        currentStreak++; // use ++ in preference to +=1, and this should be before maxRun test
-		        if (maxStreak < currentStreak) {
-		            maxStreak = currentStreak;
-		            streakStart = (i-1); // this will produce a 1-based position
-		        } // end of if
-		    } else { // sets current run back to zero in order to count the next run
-		        currentStreak = 0;
-		    } // end of else
+				if (diceArray[i] == diceArray[i-1]) { // never compare a boolean with a boolean constant, just use it
+					currentStreak++; // use ++ in preference to +=1, and this should be before maxRun test
+					if (maxStreak < currentStreak) {
+						maxStreak = currentStreak;
+						streakStart = (i-currentStreak + 1); // this will produce a 1-based position
+					} // end of if
+				} else { // sets current run back to zero in order to count the next run
+					currentStreak = 1;
+				} // end of else
 			}
 		}// end of for loop
-		
+
 		// Print the final results back to the user
 		// I used (i-1) because the for loop adds one to i each time it loops
 		// and the last roll adds 1 to the index but doesn't create a value for diceArray[i]
@@ -105,11 +105,11 @@ public class RollTheDiceBeta {
 		System.out.printf("                              6's = " + six + " -- which is " + "%3.1f", ((double)six/(double)(numberRolls)*100));
 		System.out.println("% of the total rolls");
 
-		
+
 		System.out.println("The streak data!");
-		
+
 		System.out.println("The longest streak was a streak of " + maxStreak);
-		System.out.println("The longest streak was a streak of " + diceArray[streakStart] + "'s and it started at roll " + streakStart);
+		System.out.println("The longest streak was a streak of " + diceArray[streakStart] + "'s and it started at roll " + (streakStart));
 		// I always put this at the end of every program
 		System.out.println("\n\n\nThe program has completed successfully");
 	}
